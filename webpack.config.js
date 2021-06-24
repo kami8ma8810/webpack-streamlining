8f92491c99cdb724545ed19e73b3f5417ecccc9d
Hayato Kamiyama <kami8ma8810@gmail.com>
2021-06-24 10:14:30 +0900

    リモートからプル

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    main: './src/javascripts/main.js',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'javascripts/main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+          }
+        ],
+      },
+      {
+        test: /\.png|\.jpg/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+        use: [
+          // {
+          //   loader: 'file-loader',
+          //   options: {
+          //     esModule: false,
+          //     name: 'images/[name].[ext]',
+          //   },
+          // },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
